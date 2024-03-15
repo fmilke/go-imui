035f3761c6f63a2ba0f3c36adbf90e5c8527fe2b
fmilke <fridjofmilke@gmail.com>
2024-03-15 08:30:06 +0100

    finished basic lru and with tests

--- lru/lru.go
+++ lru/lru.go
@@ -1,22 +1,43 @@
 package lru
 
 import (
-	"github.com/danielgatis/go-freetype/freetype"
+	"encoding/json"
 )
 
-type LRUCacheEntry struct {
-	value freetype.Metrics
-	newer *LRUCacheEntry
-	older *LRUCacheEntry
+
+type Entries[T any] map[rune]*LRUCacheEntry[T]
+
+type LRUCacheEntry[T any] struct {
+    key rune
+	value T
+	newer *LRUCacheEntry[T]
+	older *LRUCacheEntry[T]
 }
 
-type LRUCache struct {
-	entries map[rune]*LRUCacheEntry
-	oldest  *LRUCacheEntry
-	newest  *LRUCacheEntry
+type LRUCache[T any] struct {
+	entries Entries[T]
+	oldest  *LRUCacheEntry[T]
+	newest  *LRUCacheEntry[T]
+    size    uint
+    maxSize uint
 }
 
-func (e *LRUCacheEntry) unplug() {
+func NewLRUCache[T any](s uint) *LRUCache[T] {
+    return &LRUCache[T] {
+        entries: make(Entries[T]),
+        oldest: nil,
+        newest: nil,
+        size: 0,
+        maxSize: s,
+    }
+}
+
+// A helper functions, that removed
+// and element from the linked list and
+// reconnects open ends
+// but the enrty itself still points
+// to its siblings and needs correction
+func (e *LRUCacheEntry[T]) unplug() {
 	older := e.older
 	newer := e.newer
 
@@ -25,48 +46,66 @@ func (e *LRUCacheEntry) unplug() {
 	}
 
 	if newer != nil {
-		newer.older = nil
+		newer.older = older
 	}
 }
 
-func (cache *LRUCache) get(key rune) *freetype.Metrics {
+func (cache *LRUCache[T]) Get(key rune) *T {
 	e := cache.entries[key]
 
-	if e == nil || cache.newest == e {
-		return nil
+	if e == nil {
+        var r T
+		return &r
 	}
+
+    if e == cache.newest {
+        return &e.value
+    }
 
 	e.unplug()
 
 	e.newer = nil
-	if cache.newest != nil {
-		e.older = cache.newest
-		cache.newest.newer = e
-		cache.newest = e
-	} else {
-		e.older = nil
-		cache.newest = e
-	}
+    e.older = cache.newest
+    cache.newest.newer = e
+    cache.newest = e
 
 	return &e.value
 }
 
-func (cache *LRUCache) store(key rune, metrics freetype.Metrics) {
-	old := cache.entries[key]
+func (cache *LRUCache[T]) Store(key rune, v T) {
+    e := cache.entries[key]
 
-	e := &LRUCacheEntry{
-		value: metrics,
-		older: cache.newest,
-	}
+    if e != nil {
+        e.unplug()
+    } else {
+        e = &LRUCacheEntry[T] {
+            value: v,
+            older: cache.newest,
+            key: key,
+        }
 
-	if old != nil {
-		old.unplug()
-	}
+        // increase counter 
+        // or remove oldest one
+        if cache.size < cache.maxSize {
+            cache.size += 1
+        } else if cache.oldest != nil {
+            oldest := cache.oldest
 
-	cache.entries[key] = e
+            if oldest.newer != nil {
+                cache.oldest = oldest.newer
+                cache.oldest.newer = nil
+            }
 
-	if cache.newest != nil {
-		cache.newest.newer = e
-	}
-	cache.newest = e
+            delete(cache.entries, oldest.key)
+        }
+
+        cache.entries[key] = e
+    }
+
+    cache.newest = e
+
+    if cache.oldest == nil {
+        cache.oldest = e
+    }
 }
+
